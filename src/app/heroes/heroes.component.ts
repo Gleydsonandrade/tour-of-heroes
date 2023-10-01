e7f10407833ff03cd7d0eac7a0aa4a96fe6e3cc7
Gleydsonandrade <140664616+Gleydsonandrade@users.noreply.github.com>
2023-10-01 17:35:16 -0300

    atualizaçao

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
-   hero: Hero = {
-    id: 1,
-    name: 'wolverine',
-   } ;
+export class HeroesComponent  {
+   
 
-   constructor() { }
-    ngOnInit(): void {
+   heroes = HEROES;
+   selectedHero?: Hero;
 
-    }
+   onSelect(hero: Hero): void {
+     this.selectedHero = hero;
+   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Wolverine'},
+  {id: 2, name: 'Hulk'},
+  {id: 3, name: 'Homem-Aranha'},
+  {id: 4, name: 'Homem de Ferro'},
+  {id: 5, name: 'Super-Man'},
+  {id: 6, name: 'Capitao-America'},
+  {id: 7, name: 'Doutor Estranho'},
+  {id: 8, name: 'Venom'},
+  {id: 9, name: 'Mulher-Maravilha'},
+  {id: 10, name: 'Batman'},
+];
